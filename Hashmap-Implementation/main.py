99e7edf7362ed68492cf74975d01d680db4a55b9
HaroldDagadu <121590606+HaroldDagadu@users.noreply.github.com>
2024-03-24 12:27:56 +0000

    Update main.py
    daga wuz here s)m

---
+++ Hashmap-Implementation/main.py
@@ -0,0 +1,63 @@
+dictionary_words = {
+    "apple": "a round fruit with red or green skin and a whitish interior",
+    "banana": "a long curved fruit that grows in clusters and has soft pulpy flesh and yellow skin when ripe",
+    "carrot": "a tapering orange-colored root eaten as a vegetable",
+    "dog": "a domesticated carnivorous mammal that typically has a long snout, an acute sense of smell, and a barking, howling, or whining voice",
+    "elephant": "a very large herbivorous mammal with a long, flexible trunk, large ears, and tusks",
+    "flower": "the seed-bearing part of a plant, consisting of reproductive organs (stamens and carpels) that are typically surrounded by a brightly colored corolla (petals) and a green calyx (sepals)",
+    "guitar": "a stringed musical instrument with a fretted fingerboard, typically incurved sides, and six or twelve strings, played by plucking or strumming with the fingers or a plectrum",
+    "house": "a building for human habitation, especially one that is lived in by a family or small group of people",
+    "ice cream": "a sweetened frozen food typically eaten as a snack or dessert",
+    "jazz": "a type of music of black American origin characterized by improvisation, syncopation, and usually a regular or forceful rhythm",
+    "kangaroo": "a large plant-eating marsupial with a long powerful tail and strongly developed hind limbs that enable it to travel by leaping, found only in Australia and New Guinea",
+    "laptop": "a portable computer that can be used on one's lap",
+    "mountain": "a large natural elevation of the earth's surface rising abruptly from the surrounding level; a large steep hill",
+    "notebook": "a book with blank pages for recording notes or memoranda",
+    "ocean": "a very large expanse of sea, in particular, each of the main areas into which the sea is divided geographically",
+    "piano": "a large keyboard musical instrument with a wooden case enclosing a soundboard and metal strings, which are struck by hammers when the keys are depressed",
+    "queen": "the female ruler of an independent state, especially one who inherits the position by right of birth",
+    "rainbow": "an arc of colors formed in the sky opposite the sun by the refraction and reflection of the sun's rays in raindrops",
+    "sunflower": "a tall North American plant of the daisy family, with very large golden-rayed flowers. Sunflowers are cultivated for their edible seeds, which are an important source of oil for cooking and margarine",
+    "tree": "a woody perennial plant, typically having a single stem or trunk growing to a considerable height and bearing lateral branches at some distance from the ground",
+    "umbrella": "a device consisting of a circular canopy of cloth on a folding metal frame supported by a central rod, used as protection against rain or sometimes sun",
+    "violin": "a stringed musical instrument of treble pitch, played with a horsehair bow. The classical European violin was developed in the 16th century. It has four strings and a body of characteristic rounded shape, narrowed at the middle and with two f-shaped sound holes",
+    "waterfall": "a cascade of water falling from a height, formed when a river or stream flows over a precipice or steep incline",
+    "xylophone": "a musical instrument played by striking a row of wooden bars of graduated length with one or more small wooden or plastic mallets",
+    "yacht": "a medium-sized sailboat equipped for cruising or racing",
+    "zebra": "an African wild horse with black-and-white stripes and an erect mane",
+}
+
+
+image_hash_table = {
+    "apple": "apple_image.jpg",
+    "banana": "banana_image.WEBP",
+    "carrot": "carrot_image.jpg",
+    "dog": "dog_image.jpg",
+    "elephant": "elephant_image.jpg",
+    "flower": "flower_image.jpg",
+    "guitar": "guitar_image.jpg",
+    "house": "house_image.jpg",
+    "ice cream": "ice_cream_image.jpg",
+    "jazz": "jazz_image.jpg",
+    "kangaroo": "kangaroo_image.jpg",
+    "laptop": "laptop_image.jpg",
+    "mountain": "mountain_image.jpg",
+    "notebook": "notebook_image.jpg",
+    "ocean": "ocean_image.jpg",
+    "piano": "piano_image.jpg",
+    "queen": "queen_image.jpg",
+    "rainbow": "rainbow_image.jpg",
+    "sunflower": "sunflower_image.jpg",
+    "tree": "tree_image.jpg",
+    "umbrella": "umbrella_image.jpg",
+    "violin": "violin_image.jpg",
+    "waterfall": "waterfall_image.jpg",
+    "xylophone": "xylophone_image.jpg",
+    "yacht": "yacht_image.jpg",
+    "zebra": "zebra_image.jpg",
+}
+
+synonyms={
+          
+    
+}
